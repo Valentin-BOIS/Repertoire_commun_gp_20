8925c096c218c067929b28667dd2bd13a8e085c8
valentinbois <valentin.bois@eleve.ensai.fr>
2026-09-11 14:54:48 +0000

    Travaux sur le TPn°3+++

--- backend/src/sandbox.py
+++ backend/src/sandbox.py
@@ -2,6 +2,7 @@
 from business_object.game import Game
 from business_object.player import Player
 from dao.game_dao import GameDao
+from dao.player_dao import PlayerDao
 from utils.env_variables import load_environment_variables
 from datetime import datetime
 
@@ -11,6 +12,6 @@ p1 = Player(username="Toto", password="Toto12", elo="12", email="toto@gmail.com"
 p2 = Player(username="Tata", password="Tata12", elo="21", email="tata@gmail.com", pokemon_fan="FALSE")
 game = Game(player1=p1, player2=p2, game_mode="dice", winner=p1, description="blabla", timestamp=datetime.now(), id_game = "45678")
 
-id = GameDao().create(p1)
+id = PlayerDao().create(p1)
 print(id)
 game2 = GameDao().find_by_id(45678)
